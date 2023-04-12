2b2e4e5f1b1559bf35002e8dc3689023ba664560
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2023-04-12 23:05:48 +0200

    Add listing of available content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import {Attributes as PageNode} from "@/types/schemas/node--page";
+import {Attributes as ArticleNode} from "@/types/schemas/node--article";
 import {Drupal, DrupalJsonApiParams} from "@/lib/drupal";
+import Link from "next/link";
 
 const params = DrupalJsonApiParams();
 
@@ -10,10 +12,23 @@ export default async function Home() {
         params: params
     }) as PageNode;
 
+    const articles = await Drupal.getObject({
+        objectName: 'node--article',
+        params,
+    }) as Array<ArticleNode>
+
     return (
         <>
             <h1>{node.title}</h1>
             <p>{node.body?.value}</p>
+            <ul>
+                {articles.map((item) => {
+                    return (
+                        <li key={item.path?.alias}><Link
+                            href={item.path?.alias || `/node/${item.drupal_internal__nid}`}>{item.title}</Link></li>
+                    )
+                })}
+            </ul>
         </>
     )
 }
