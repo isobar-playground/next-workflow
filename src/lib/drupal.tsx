82ec6c67a73165cab9bf2a70f63d57593f6792e9
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2023-04-14 11:45:50 +0200

    Force noStore parameter to true.

--- src/lib/drupal.tsx
+++ src/lib/drupal.tsx
@@ -5,7 +5,7 @@ export const ResourceVersion = process.env.RESOURCE_VERSION || 'latest-version';
 export const Drupal = new DrupalState({
     apiBase: process.env.DRUPAL_BASE_URL || '',
     debug: (process.env.ENVIRONMENT || 'production') === 'development',
-    noStore: (process.env.ENVIRONMENT || 'production') === 'development',
+    noStore: true,
 })
 export const DrupalJsonApiParams = () => {
     const DrupalJsonApiParams = new BaseDrupalJsonApiParams();
